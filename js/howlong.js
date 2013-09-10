a51a784ec86d3a78dcc4022efff658f14d0faade
apatkinson <apatkinson@gmail.com>
2013-09-11 00:04:49 +0100

    new game format complete
    no progression and timers yet.

--- js/howlong.js
+++ js/howlong.js
@@ -1,50 +1,68 @@
-function MathsGame(amountQuestions) {
+function MathsGame() {
     this.questions = [];
-    this.amountQuestions = amountQuestions;
     this.currentQuestion = 0;
     this.wrongAnswers = [];
-    this.started = 0;
-    this.finished = 0;
-    
-    for (var i = 0; i < this.amountQuestions; i++) {
-        this.questions.push(new Question());
-    }
 }
 
+/**
+ * Start the game, write a new question to the screen.
+ */
 MathsGame.prototype.start = function() {
-    if (!this.started) {
-        $('#challenge label').html(this.questions[this.currentQuestion].questionString());
-        this.started = this.getCurrentSeconds();
-    }
+        this.writeNewQuestion();
 };
 
-MathsGame.prototype.saveAnswer = function() {
-    this.questions[this.currentQuestion].userAnswer = parseInt($('#challenge input').val());
-    if (isNaN(this.questions[this.currentQuestion].userAnswer)) { 
+/**
+ * check the users answer is a number and
+ * check to see if it is the correct answer 
+ */
+MathsGame.prototype.checkAnswer = function() {
+    if (this.questions[this.questions.length - 1].userAnswer != this.questions[this.questions.length - 1].answer) {
+        console.log('wrong answer, oops');
+        this.finish();
+        
         return false;
     }
-    $('#challenge input').val('');
+    if (isNaN(this.questions[this.questions.length - 1].userAnswer)) { 
+        
+        return false;
+    }
+    
     return true;
+}
+
+/**
+ * save the answer that the user types.
+ */
+MathsGame.prototype.saveAnswer = function() {
+    this.questions[this.questions.length - 1].userAnswer = parseInt($('#challenge input').val());
 };
 
-MathsGame.prototype.newQuestion = function() {
-    if (this.saveAnswer()) {
-        if (this.currentQuestion < this.amountQuestions - 1) {
-            this.currentQuestion ++;
-            $('#challenge label').html(this.questions[this.currentQuestion].questionString());
+/**
+ * clear the current question and values
+ * and then write a new question to the page.
+ */
+MathsGame.prototype.writeNewQuestion = function() {
+    $('#challenge input').val('');
+    this.questions.push(new Question());
+    $('#challenge label').html(this.questions[this.currentQuestion].questionString());
+    this.currentQuestion++;
+};
 
-        } else {
-            if (!this.finished) {
-                this.finished = this.getCurrentSeconds();
-                this.finish();
-            }
-        }
-    } else {
-        //should probably alert the user and not just the developer!
-        console.log('Please enter a number');
+/**
+ * after the game has started use next question to reset everything, validate the
+ * users input and write the next question.
+ */
+MathsGame.prototype.nextQuestion = function() {
+    this.saveAnswer();
+    if (this.checkAnswer()) {
+        this.writeNewQuestion(); 
     }
 };
 
+/**
+ * at any point you can finish the game, and this hides everything and shows
+ * the score you got.
+ */
 MathsGame.prototype.finish = function() {
     $('#challenge').hide();
     var results = '';
@@ -54,24 +72,28 @@ MathsGame.prototype.finish = function() {
             results = results + '#'+(i+1) + ' - ' + this.questions[i].questionString() + ' = ' + this.questions[i].answer + '. Your answer: ' + this.questions[i].userAnswer + '<br>';
         }
     }
-    results = results + '<br> You scored ' + (this.amountQuestions-this.wrongAnswers.length) + ' out of ' + this.amountQuestions;
-    results = results + '<br> It took you ' + ((this.finished-this.started)/1000) + ' seconds';
+    results = results + '<br> NOPE, ' + this.questions[(this.currentQuestion-1)].questionString() + ' is ' + this.questions[(this.currentQuestion-1)].answer;
+    results = results + '<br> You put: ' + this.questions[(this.currentQuestion-1)].userAnswer;
+    results = results + '<br> You scored: ' + (this.currentQuestion-1);
     $('#result').html(results);
 };
 
+/**
+ * get current seconds of the system
+ */
 MathsGame.prototype.getCurrentSeconds = function() {
     return new Date().getTime();
 }
 
 $(function(){
-    var mathsGame = new MathsGame(10);
+    var mathsGame = new MathsGame();
 
     $("body").keypress(function(e) {
         if (e.which == 32) {
             mathsGame.start();
         }
         if (e.which == 13) {
-            mathsGame.newQuestion();
+            mathsGame.nextQuestion();
         }
     });
 });
